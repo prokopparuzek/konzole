e1760d857fbed8a72b11a615bc62a509f7ae106d
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-05-25 18:35:19 +0200

    lcd test

--- lcd/test.ino
+++ lcd/test.ino
@@ -1,15 +1,22 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-    lcd.init();
-    lcd.backlight();
-    lcd.setCursor(0,0);
-    lcd.print("AHOJ Bimbo!");
+  lcd.init();
+  lcd.backlight();
+  lcd.cursor();
+  lcd.home();
+  lcd.print("AHOJ");
+  lcd.leftToRight();
+  lcd.setCursor(0, 1);
+  for (int i = 0; i < 10; i++) {
+    lcd.print(i);
+    delay(500);
+  }
+  lcd.autoscroll();
 }
 
-void loop() {
-}
+void loop() {}
