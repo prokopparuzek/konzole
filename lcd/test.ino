fc861cc5ee05648df9d2efcf36b5b1484982381a
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-05-27 18:01:41 +0200

    chartest lcd

--- lcd/test.ino
+++ lcd/test.ino
@@ -5,18 +5,17 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
+  Serial.begin(115200);
   lcd.init();
   lcd.backlight();
-  lcd.cursor();
   lcd.home();
-  lcd.print("AHOJ");
-  lcd.leftToRight();
-  lcd.setCursor(0, 1);
-  for (int i = 0; i < 10; i++) {
-    lcd.print(i);
-    delay(500);
-  }
-  lcd.autoscroll();
 }
 
-void loop() {}
+void loop() {
+  if (Serial.available()) {
+    String s = Serial.readString();
+    uint16_t i = s.toInt();
+    lcd.write(i);
+    lcd.home();
+  }
+}
